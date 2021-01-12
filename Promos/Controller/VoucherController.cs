319a1b598a83a69f4741a182e97acea4351fc40d
rosiq2000 <61879891+rosiq2000@users.noreply.github.com>
2021-01-12 10:32:09 +0700

    Add files via upload

---
+++ Promos/Controller/VoucherController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Promos.Model;
+
+namespace Promos.Controller
+{
+    class Payment
+    {
+        private List<Voucher> items;
+
+        public Payment()
+        {
+            items = new List<Voucher>();
+        }
+
+        public void addItem(Voucher item)
+        {
+            this.items.Add(item);
+        }
+
+        public List<Voucher> getItems()
+        {
+            return this.items;
+        }
+
+    }
+}
